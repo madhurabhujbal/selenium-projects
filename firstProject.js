aa2aaa0cc50155c1a8e2fb35dc0d72e15a12c37b
Madhura <madhura.bhujbal@gmail.com>
2020-10-07 22:59:16 +0100

    Add test code to open Google page

--- firstProject.js
+++ firstProject.js
@@ -1,2 +1,7 @@
+var webdriver = require("selenium-webdriver");
 
+var browser_name = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
 
+browser_name.get("http:/www.google.com");
